026d07e9e137846439c732a7e5ad11972d19d5dc
erickmarca13 <132939273+erickmarca13@users.noreply.github.com>
2023-05-15 17:30:24 -0300

    11
    1212

--- JS/15-5.js
+++ JS/15-5.js
@@ -1,6 +1,13 @@
+let= num =545
 function ejemplo(params) {
    let inp = document.querySelector("#entrada");
+   let p = document.querySelector("#salida")
+   let diving = document.querySelector("#img")
+   diving.innerHTML = '<img src ="../img/Gato.jpeg" alt="error"/>';
    let valor = inp.value;
+   p.innerText = valor; 
+   console.log(p.value);
    console.log(valor);
-   console.log(inp);
+   console.dir(inp);
+   console.log(num)
 }
