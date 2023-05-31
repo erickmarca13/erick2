46469a78b97726a6f1c3d50f0f43d279f25ad3f3
erickmarca13 <132939273+erickmarca13@users.noreply.github.com>
2023-05-31 14:46:41 -0300

    tp
    push dos del 31 de mayo

--- JS/tp.js
+++ JS/tp.js
@@ -5,3 +5,17 @@ function Prueba(params) {
         
     }
 }
+function Ejercicio2() {
+    var valores = [true, 5, false, "hola", "adios",2,"gjdjdflk"];
+    var valor = "";
+
+    for (let i = 0; i < valores.length; i++) {
+        if (typeof(valores[i]) == "string") {
+            if (valores[i].length > valor.length) {
+            valor = valores[i];
+        }
+    }
+}
+
+    console.log("el mayor es "+valor);
+}
